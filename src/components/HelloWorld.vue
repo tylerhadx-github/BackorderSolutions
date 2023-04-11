0b80eebf0d8638e0496d625c365b39e87899477c
Tyler Haddix <thaddix@raptortech.com>
2023-04-11 15:23:17 -0600

    updated

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,30 @@
 <template>
   <v-container class="fill-height">
-    
     <v-responsive class="d-flex align-center text-center fill-height">
       <div class="font-weight-medium mb-n1">Welcome to</div>
       <v-img contain height="400" src="@/assets/intiallogo.jpg" />
       <div class="py-1" />
 
       <div class="font-weight-bold mb-n1">
-        Where we specialize in providing solutions for disposal items on
-        backorder in the medical field. Our team works with manufacturers and
-        suppliers worldwide to ensure that you have access to the products you
-        need in a timely manner.<br /><br />
+        Backorder Solutions is a US-based company dedicated to providing medical
+        facilities with effective solutions amidst current supply constraints.
+        Through our partnership with a trusted manufacturing company boasting
+        over two decades of experience in producing pneumatic tourniquet cuffs,
+        we offer exceptional manufacturing equipment and capabilities, including
+        the ability to create different molds. 
+        
+        <br /><br />
+        Our products undergo Cobalt 60
+        radiation sterilization and are manufactured in a 100,000-level
+        purification workshop. We hold several patented technologies and can
+        customize our products to suit the unique needs of our clients. We
+        maintain strict adherence to ISO13485 and ISO14001 standards in all our
+        production processes.
+        <br /><br />
+        Where we specialize in providing solutions for
+        disposal items on backorder in the medical field. Our team works with
+        manufacturers and suppliers worldwide to ensure that you have access to
+        the products you need in a timely manner.<br /><br />
         If you have a product on backorder, visit our "Backorder Requests" page
         to provide us with detailed information, and our Backorder Specialists
         will work with you to find a solution. <br /><br />For any other
@@ -33,11 +47,15 @@
           </v-btn>
         </v-col>
         <v-col cols="auto">
-
-    <v-btn to="/ContactUs"              size="x-large"
- variant="flat" color="black" class="ml-2">
-      Contact Us
-    </v-btn>
+          <v-btn
+            to="/ContactUs"
+            size="x-large"
+            variant="flat"
+            color="black"
+            class="ml-2"
+          >
+            Contact Us
+          </v-btn>
         </v-col>
       </v-row>
     </v-responsive>
--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -7,11 +7,40 @@
   </v-btn>
 
     <v-app-bar-title> </v-app-bar-title>
+    <template v-slot:append>
+          <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        </template>
     <v-spacer></v-spacer>
 
   </v-app-bar>
+  <v-navigation-drawer
+        v-model="drawer"
+        location="right"
+        temporary
+      >
+        
+        <v-list-item v-for="(i,j) in items" :key="j"
+            :title="i.title"
+            :to="i.Value"
+            
+          ></v-list-item>
+      
+      </v-navigation-drawer>
 </template>
 
-<script setup>
-//
+<script >
+ export default {
+    data: () => ({
+      drawer: false,
+      items: [
+      {title: 'Home', Value: '/'},
+        {title: 'Backorder Request', Value: 'Request'},
+        {title: 'Contact Us', Value: 'ContactUs'},
+      ],
+    }),
+
+    watch: {
+      
+    },
+  }
 </script>
